b910f454d7ab803814380019641e649591082321
smartshaila <smartshaila@gmail.com>
2014-11-24 07:33:13 -0500

    Added methods to team to verify voting stage for teams/missions

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,20 @@ class Team < ActiveRecord::Base
   belongs_to :mission
   has_many :team_votes, dependent: :destroy
   has_many :team_assignments, dependent: :destroy
+
+  def assignments_complete?
+    self.team_assignments.size == MissionCapacity.find_by(player_count: self.mission.game.player_assignments.size, mission_number: self.mission.mission_number)
+  end
+
+  def team_voting_complete?
+    self.team_votes.size == self.mission.game.player_assignments.size
+  end
+
+  def approved?
+    
+  end
+
+  def mission_voting_complete?
+    self.team_assignments.all? {|ta| !ta.pass.nil? }
+  end
 end
