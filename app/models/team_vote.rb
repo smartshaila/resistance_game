8280a3e0d4e359680ec2fafe4cc759a22f516c00
smartshaila <smartshaila@gmail.com>
2014-11-26 16:21:08 -0500

    Fixed syntax for relationship between team votes and mission, games from 'belongs_to, through' to 'delegate, to'

--- app/models/team_vote.rb
+++ app/models/team_vote.rb
@@ -1,6 +1,6 @@
 class TeamVote < ActiveRecord::Base
   belongs_to :team
   belongs_to :player_assignment
-  belongs_to :mission, through: :team
-  belongs_to :game, through: :mission
+  delegate :mission, to: :team
+  delegate :game, to: :mission
 end
