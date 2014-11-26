9813a5557a6a38ea5641af20c66a2257c832f645
Icehawk78 <i.am@aweso.ms>
2014-11-26 17:06:18 -0500

    Added game ending state and fixed mission result evaluations

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -4,18 +4,22 @@ class Mission < ActiveRecord::Base
   has_many :team_assignments, through: :teams
 
   def current_team
-    self.teams.to_a.find {|team| team.team_voting_complete? and team.approved?}
+    self.teams.max
   end
 
   def rejection_count
     self.teams.to_a.count {|team| team.team_voting_complete? and !team.approved?}
   end
 
+  def voting_results
+    current_team.team_assignments.group_by(&:pass).map{|result, assignments|{result => assignments.size}}.inject({}){|res, curr| res.merge(curr)} if current_team.mission_voting_complete?
+  end
+
   def result
-    current_team.team_assignments.all? {|assignment| assignment.pass} unless (current_team.nil? or !current_team.mission_voting_complete?)
+    (voting_results[false] or 0) < capacity.fail_count unless voting_results.nil?
   end
 
   def capacity
-    MissionCapacity.find_by(player_count: self.game.player_assignments.size, mission_number: self.mission_number).first
+    MissionCapacity.find_by(player_count: self.game.player_assignments.size, mission_number: self.mission_number)
   end
 end
