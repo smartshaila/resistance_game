c6d5785855862297d3d42e4c19a320d6d40f20d9
Icehawk78 <i.am@aweso.ms>
2014-11-29 16:21:05 -0500

    Fix the stupid login bullshit from before

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,10 +1,11 @@
 class LoginController < ApplicationController
   def index
     @games = Game.all.find_all {|g| !g.complete?}
+    @player_assignment_json = Game.all.map{|g| {g.id => g.player_assignments.map{|p| {id: p.id, name: p.player.name}}}}.inject({}){|a,b| a.merge(b)}.to_json
   end
 
   def login
-    pa = params[:player_assignment_id].first
+    pa = params[:player_assignment_id]
     unless pa.empty?
       player_assignment = PlayerAssignment.find(pa)
       player = player_assignment.player
