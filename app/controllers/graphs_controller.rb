fba3d476b498d3fcdba3946a7c28ace1a1936411
smartshaila <smartshaila@gmail.com>
2015-01-23 19:40:05 -0500

    Fixed broken graphs

--- app/controllers/graphs_controller.rb
+++ app/controllers/graphs_controller.rb
@@ -5,15 +5,14 @@ class GraphsController < ApplicationController
   end
 
   def apply
-    @person = Player.find(params[:player_id])
-    logger.info "*** #@person"
-    @completed_games = Game.all.select{|g| g.complete?}.select{|g| g.players.to_a.include? @person}
+    @player = Player.find(params[:player_id]).first
+    @completed_games = Game.all.select{|g| g.complete?}.select{|g| g.players.to_a.include? @player}
 
-#    @games_by_date = @completed_games.group_by{|game|
-#      game.created_at.to_date
-#    }.map{|d,g|
-#      [d, g.count]
-#    }
+   @games_by_date = @completed_games.group_by{|game|
+     game.created_at.to_date
+   }.map{|d,g|
+     [d, g.count]
+   }
   end
 
 end
