39918086b240330cdda188f4cc799bbda9f7db38
smartshaila <smartshaila@gmail.com>
2015-02-18 16:26:38 -0500

    Added Fail Count to Game State table -- Red Superscript

--- app/helpers/player_assignments_helper.rb
+++ app/helpers/player_assignments_helper.rb
@@ -2,7 +2,7 @@ module PlayerAssignmentsHelper
   def scoreboard_table game
     capacities = ([['Mission #', 'Capacity', 'Rejected Teams', 'Result']] | game.mission_capacities.map{|c|
       mission = game.missions.where(mission_number: c.mission_number).first
-      [c.mission_number + 1, c.capacity, (mission.rejection_count unless mission.nil?), ("<span class='#{(mission.result.nil? ? 'glyphicon glyphicon-time' : (mission.result ? 'glyphicon glyphicon-ok' : 'glyphicon glyphicon-remove'))}'></span>" unless mission.nil?)]
+      [c.mission_number + 1, c.capacity.to_s + (c.fail_count > 1 ? "<sup> [#{c.fail_count}]</sup>" : ""), (mission.rejection_count unless mission.nil?), ("<span class='#{(mission.result.nil? ? 'glyphicon glyphicon-time' : (mission.result ? 'glyphicon glyphicon-ok' : 'glyphicon glyphicon-remove'))}'></span>" unless mission.nil?)]
     }).transpose
     header = capacities.first
     capacities = capacities[1..-1]
